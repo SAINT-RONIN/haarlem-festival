dfd454feb3c7dae8d01be0a0dffd6688ece0a29e
Kian Khatibi <kiankian702@gmail.com>
2026-03-10 11:39:52 +0100

    Fix: jazz detail page error.

--- app/src/Controllers/JazzController.php
+++ app/src/Controllers/JazzController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use App\Controllers\Support\ControllerErrorResponder;
+use App\Services\GumboKingsDetailService;
 use App\Services\JazzService;
 
 /**
@@ -13,10 +14,12 @@ use App\Services\JazzService;
 class JazzController extends BaseController
 {
     private JazzService $jazzService;
+    private GumboKingsDetailService $gumboKingsDetailService;
 
     public function __construct()
     {
         $this->jazzService = new JazzService();
+        $this->gumboKingsDetailService = new GumboKingsDetailService();
     }
 
     /**
@@ -27,6 +30,19 @@ class JazzController extends BaseController
         try {
             $viewModel = $this->jazzService->getJazzPageData();
             $this->renderPage(__DIR__ . '/../Views/pages/jazz.php', $viewModel);
+        } catch (\Throwable $error) {
+            ControllerErrorResponder::respond($error);
+        }
+    }
+
+    /**
+     * Display Gumbo Kings artist detail page.
+     */
+    public function gumboKings(): void
+    {
+        try {
+            $viewModel = $this->gumboKingsDetailService->getPageData();
+            require __DIR__ . '/../Views/pages/jazz-gumbo-kings.php';
         } catch (\Throwable $error) {
             ControllerErrorResponder::respond($error);
         }
