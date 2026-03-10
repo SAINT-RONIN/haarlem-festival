116c0d94dc1791f82d8c22d7c61ec257c65748af
Kian Khatibi <kiankian702@gmail.com>
2026-03-10 16:44:23 +0100

    Fix: admin dashbaord errors and Yummy page errors.

--- app/src/Services/Interfaces/ICmsService.php
+++ app/src/Services/Interfaces/ICmsService.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\Services\Interfaces;
 
+use App\ViewModels\GlobalUiData;
+use App\ViewModels\HeroData;
+
 interface ICmsService
 {
     public function getHomePageContent(): array;
@@ -12,8 +15,12 @@ interface ICmsService
 
     public function getHeroSectionContent(string $pageSlug): array;
 
+    public function buildHeroData(string $pageSlug, string $currentPage): HeroData;
+
     /**
      * @return array{content: array, isLoggedIn: bool}
      */
     public function getGlobalUiContent(): array;
+
+    public function buildGlobalUiData(): GlobalUiData;
 }
